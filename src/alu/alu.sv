775230aa382aa9ee62a7c404c8bebce94ee7ee63
Robert Zieba <robert@zieba.site>
2023-02-16 21:55:00 -0700

    Add alu skeleton and tests

---
+++ src/alu/alu.sv
@@ -0,0 +1,8 @@
+import p_alu::*;
+
+module m_alu(
+	input s_control control,
+	output[31:0] result,
+	output e_cmp_res cmp_res
+);
+endmodule
---
+++ src/alu/shifter.sv
@@ -0,0 +1,23 @@
+import p_common::*;
+
+module m_shifter(
+	input[31:0] data,
+	input s_shift shift,
+
+	output[31:0] shifted
+);
+
+	assign shifted = f_shift(data, shift);
+
+	function[31:0] f_shift(input[31:0] d, input s_shift s);
+		case (s.shift_type)
+			SHIFT_SHL: f_shift = d << s.amount;
+			SHIFT_SHR: f_shift = d >> s.amount;
+
+			SHIFT_ASL: f_shift = d <<< s.amount;
+			SHIFT_ASR: f_shift = d >>> s.amount;
+
+			default: f_shift = d;
+		endcase
+	endfunction
+endmodule
